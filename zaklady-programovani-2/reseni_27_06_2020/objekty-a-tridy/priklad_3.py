1b3fcc4c66a2bb92b286bdc2035aa947ba02767c
Jiří Pešík <jiri.pesik@gmail.com>
2021-01-24 13:00:23 +0100

    Update priklad_3.py

--- zaklady-programovani-2/reseni_27_06_2020/objekty-a-tridy/priklad_3.py
+++ zaklady-programovani-2/reseni_27_06_2020/objekty-a-tridy/priklad_3.py
@@ -1,11 +1,11 @@
 """
 U zaměstnanců budeme nově evidovat, jestli jsou ve zkušební době.
 
-Rozšiřte funkci __init__ třídy Employee o parametr probation, který bude typu bool. Tuto hodnotu uložte jako atribut třídy Employee.
+Rozšiř funkci __init__ třídy Employee o parametr probation, který bude typu bool. ¨
+Tuto hodnotu ulož jako atribut třídy Employee.
 
-Upravte funkci __init__ třídy Manager. Hodnotu parametru probation předejte funkci __init__ třídy Employee.
-
-Upravte funkci getInfo. Pokud je zaměstnanec ve zkušební době, přidát k jeho/jejímu výpisu text Je ve zkušební době.
+Uprav funkci getInfo. Pokud je zaměstnanec ve zkušební době, 
+přidej k jeho/jejímu výpisu text Je ve zkušební době.
 """
 
 class Employee:
@@ -25,14 +25,3 @@ class Employee:
     self.position = position
     self.probation = probation
     self.remainingHolidayDays = 25
-
-class Manager(Employee):
-  def addEmployee(self, employee):
-    self.employeeList.append(employee)
-  def getEmployees(self):
-    print("Vypisuji podřízené:")
-    for employee in self.employeeList:
-      print(employee.name)
-  def __init__(self, name, position, probation):
-    super().__init__(name, position, probation)
-    self.employeeList = []
